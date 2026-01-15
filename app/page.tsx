4735eea7bca306abb7028887ca057d737f82f23a
camelia <camelia.96str@gmail.com>
2026-01-15 11:42:27 +0100

    page clean

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { prisma } from "@/lib/prisma";
+
+export default async function Home() {
+
+  return (
+    <div className="">
+      <main className="w-5xl text-2xl">
+        
+      </main>
+    </div>
+  );
+}
