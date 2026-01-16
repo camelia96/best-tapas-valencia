8531897886d759f72744699c61969f37b3597acd
camelia <camelia.96str@gmail.com>
2026-01-16 13:50:48 +0100

    tags GET

---
+++ app/api/tags/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const tags = await prisma.tags.findMany({
+      orderBy: { id: "asc" },
+    });
+
+    return NextResponse.json({
+      status: 200,
+      success: true,
+      count: tags.length,
+      data: tags,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      error: "Internal Server Error",
+      status: 500,
+    });
+  }
+}
